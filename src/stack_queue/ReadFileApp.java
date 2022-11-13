bef432b6af1c86ad43e48383614f4eac43a3ed1e
katerinamant <katerinamantaraki@gmail.com>
2022-11-13 17:55:34 +0200

    Read maze data input from file and initialize maze array

--- src/stack_queue/ReadFileApp.java
+++ src/stack_queue/ReadFileApp.java
@@ -1,6 +1,5 @@
 package stack_queue;
 
-import java.util.*;
 import java.io.*;
 
 public class ReadFileApp {
@@ -11,54 +10,119 @@ public class ReadFileApp {
 	 */
 	private int n, m;
 	private int entrance_x, entrance_y;
-	private int[][] maze;
+	private char[][] maze;
 
-	public void ReadFile(String file) {
+	public void ReadFile(String file) throws Exception {
 		BufferedReader reader = null;
 		System.out.printf("[Reading from file: %s]\n", file);
+		int linenum = 0;
+		reader = new BufferedReader(new FileReader(file));
+		String line;
 
-		try {
-			reader = new BufferedReader(new FileReader(file));
-			String line;
-			int linenum = 0;
-
-			// Read Maze Dimensions and Entrance Coordinates
-			// Both stored in lines with format: "intA intB"
-			int[][] mazeData = new int[2][2];
-			for (int i=0; i<2; i++) {
+		// Read Maze Dimensions and Entrance Coordinates
+		// Both stored in lines with format: "intA intB"
+		int[][] mazeData = new int[2][2];
+		for (int i=0; i<2; i++) {
+			line = reader.readLine();
+			linenum++;
+			while (line.length() == 0) {
+				// Skip empty lines
+				System.out.printf("Empty line found. Ignoring line %d...\n", linenum);
 				line = reader.readLine();
 				linenum++;
-				while (line == null) {
-					// Skip empty lines
-					System.out.printf("Empty line found. Ignoring line %d...", linenum);
-					line = reader.readLine();
-					linenum++;
-				}
-				line.trim();
-				String split[] = line.split(" ");
-				if (split.length != 2) {
-					reader.close();
-					throw new IOException(String.format("Error at line %d when reading maze data", linenum));
-				}
-				mazeData[i][0] = Integer.parseInt(split[0]);
-				mazeData[i][1] = Integer.parseInt(split[1]);
+			}
+			line.trim();
+			String split[] = line.split(" ");
+			if (split.length != 2) {
+				reader.close();
+				throw new IOException(String.format("Line %d - Invalid maze data", linenum));
+			}
+			mazeData[i][0] = Integer.parseInt(split[0]);
+			mazeData[i][1] = Integer.parseInt(split[1]);
+		}
+
+		n = mazeData[0][0];
+		m = mazeData[0][1];
+		entrance_x = mazeData[1][0];
+		entrance_y = mazeData[1][1];
+
+		// Initialize maze array
+		maze = new char[n][m];
+
+		// Read maze content
+		int i = 0;
+
+		// Read all maze rows
+		while (i<n) {
+			line = reader.readLine();
+			linenum++;
+			while (line.length() == 0) {
+				// Skip empty lines
+				System.out.printf("Empty line found. Ignoring line %d...\n", linenum);
+				line = reader.readLine();
+				linenum++;
 			}
 
-			n = mazeData[0][0];
-			m = mazeData[0][1];
-			entrance_x = mazeData[1][0];
-			entrance_y = mazeData[1][1];
+			// Read all cells inside a maze row
+			line.trim();
+			char[] cells = line.toCharArray();
+			int j = 0;
+			for (char c : cells) {
+				if (c == ' ') {
+					continue;
+				}
+				// Store cell and increment j
+				if (c != '0' && c != '1' && c != 'E') {
+					reader.close();
+					throw new IOException(String.format("Line %d - Invalid maze content ('%c' is not a valid cell)", linenum, c));
+				}
 
-			// Read Maze Contents
+				// Check that row does not have extra cells
+				if (j >= m) {
+					reader.close();
+					throw new IOException(String.format(
+							"Line %d - Exceeded maze dimensions (Row %d already has %d cells)",
+							linenum, i, m));
+				}
 
-			// Pad Maze with -1s
-			maze = new int[n+2][m+2];
+				maze[i][j++] = c;
+			}
 
-			reader.close();
-		} catch (IOException e) {
-			System.out.printf("[Error when reading from file: %s]\nException: [%s]", file, e);
-		} catch (NumberFormatException e) {
-			System.out.printf("[Error when reading from file: %s]\nException: [%s]", file, e);
+			// Check that row does not have less cells than expected
+			if (j < m) {
+				reader.close();
+				throw new IOException(String.format(
+						"Line %d - Invalid maze content (Expected %d cells in row %d, found %d)",
+						linenum, m, i, j));
+			}
+
+			// Increment row count
+			i++;
 		}
+
+		// Check if entrance is at given coordinates
+		if (maze[entrance_x][entrance_y] != 'E') {
+			reader.close();
+			throw new IOException(
+					String.format(
+							"Invalid maze entrance coordinates (expected 'E' at (%d, %d), '%c' found)",
+							entrance_x,
+							entrance_y,
+							maze[entrance_x][entrance_y]));
+		}
+
+		// Check for extra lines at end of file
+		while ( (line = reader.readLine()) != null) {
+			if (line.length() != 0) {
+				reader.close();
+				throw new IOException(String.format("Line %d - More data than expected", linenum));
+			}
+		}
+		reader.close();
+		System.out.printf("[Succesfully read from file: %s]\n", file);
+	}
+
+	public char[][] getMaze() {
+		return maze;
 	}
 }
